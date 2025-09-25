b24bf7f26ade3d18f0d8c834d2539937ac544b10
library04 <leerokchan@gmail.com>
2025-09-25 11:34:29 +0900

    commit

--- test.c
+++ test.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 
-int main(void)
+#define N 5
+int main()
 {
-   int n;
-   printf("몇 단 사각형입니까?:");
-   scanf("%d", &n);
-   for (int i = 1; i <= n; i++)
-   {
-    for (int j = 1; j <= i; j++)
-        printf("*");
-        printf("\n");
+   int foo[N];
+   for (int i = 0; i < N; i++) {
+      printf("foo[%d] : ", i);
+      scanf("%d", &foo[i]);
+   }
+   puts("각 요소의 값");
+   for (int i = 0; i < 3; i++) {
+      printf("foo[%d] = %d\n", i, foo[i]);
    }
 
+   return 0;
+}
+
+#include <stdio.h>
+int main()
+{
+   int a[5] = {1, 2, 3, 4, 5};
+   int na = sizeof(a) / sizeof(a[0]);
+   printf("배열 a의 요소 개수는 %d입니다.\n", na);
+
+   for (int i = 0; i < na; i++) {
+      printf("a[%d] = %d\n", i, a[i]);
+   }
    return 0;
 }
